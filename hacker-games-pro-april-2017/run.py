f585d43deb818c41fbbb61dd07c61ceb45cff371
Sam Anthony <sam_anthony99@hotmail.com>
2017-04-02 14:39:28 +0300

    update json script.

--- hacker-games-pro-april-2017/run.py
+++ hacker-games-pro-april-2017/run.py
@@ -13,11 +13,20 @@ import sys
 import numpy as np
 import json
 
-def dataframe_to_json(df):
+def dataframe_to_json_print(df):
     json_string = df.to_json(orient = 'records', force_ascii=False)
     json_object = json.loads(json_string)
     return json.dumps(json_object, indent=4, sort_keys=True, ensure_ascii=False)
 
+def dataframe_to_json_file(df):
+    df.to_json('data-v3.json', orient = 'records', force_ascii=False)
+
+
+def format_dict(dict, arg):
+    ndict = {}
+    for k, v in dict.items():
+        ndict[ k.format(arg) ] = v.format(arg)
+    return ndict
 
 df_schools = pd.read_csv('schools.csv', sep=";")
 
@@ -36,7 +45,10 @@ df_schools.rename(inplace=True, columns={'ID': 'municipality_internal_id',
 
 df_prefs = pd.read_csv('prefs.csv', sep=";", true_values=["Taip"], false_values=["Ne"])
 
-prefs_columns = { 'Nr.': 'some_number',
+#Fix error in CSV file.
+df_prefs.rename(inplace=True, columns={'Tinkamų grupių 13darželyje': 'Tinkamų grupių 3 darželyje'})
+
+columns = { 'Nr.': 'some_number',
            'Registracijos numeris': 'registration_number',
            'Prašymo pateikimo data': 'application_data',
            'Vaiko Identifikacinis Nr.': 'student_ident',
@@ -50,26 +62,58 @@ prefs_columns = { 'Nr.': 'some_number',
            }
 
 
-# for i in range(1,6):
-#     columns.update (  {'1 pasirinktas darželis':'preference_1_school_name',
-#            '1 darželio Seniūnija':'preference_1_school_district',
-#            '1 darželio grupės ugdomoji kalba':'preference_1_school_language',
-#            '1 darželio grupės ugdymo metodika':'preference_1_some_other_info',
-#            '1 darželio grupės tipas':'preference_1_age_group',
-#            '1 darželio grupės amžiaus intervalas':'preference_1_age_interval',
-#            'Atitinka 1 darželiui priskirta teritorija':'preference_1_same_territory',
-#            '1 darželį lanko broliai/seserys':'preference_1_sibling_at_school',
-#            'Tinkamų grupių 1 darželyje':'preference_1_if_fit_group'}
-#  )
-#
-# print (columns)
+dynamic_columns_template = {'{} pasirinktas darželis': 'preference_{}_school_name',
+           '{} darželio Seniūnija':'preference_{}_school_district',
+           '{} darželio grupės ugdomoji kalba':'preference_{}_school_language',
+           '{} darželio grupės ugdymo metodika':'preference_{}_some_other_info',
+           '{} darželio grupės tipas':'preference_{}_age_group',
+           '{} darželio grupės amžiaus intervalas':'preference_{}_age_interval',
+           'Atitinka {} darželiui priskirta teritorija':'preference_{}_same_territory',
+           '{} darželį lanko broliai/seserys':'preference_{}_sibling_at_school',
+           'Tinkamų grupių {} darželyje':'preference_{}_if_fit_group'}
+
+for i in range(1,6):
+    columns.update (format_dict(dynamic_columns_template, i))
 
 
-df_prefs.rename(inplace=True, columns=prefs_columns)
 
-df_prefs = df_prefs[ prefs_columns.values() ]   #TODO quick work around, might not be needed later
+def myfunc(row):
+    row['preferences'] = []
 
-print ( dataframe_to_json(df_prefs) )
+    # print row
+    # sys.exit()
+
+    for i in range(1, 6):
+        d = format_dict(dynamic_columns_template, i)
+        preferences_dict = {}
+        for jk, jv in d.items():
+
+            # print jv
+            # print jv
+            # sys.exit()
+
+            preferences_dict[jv[13:] ] = row[jv]
+        row['preferences'].append(preferences_dict)
+
+    return row
+
+
+
+df_prefs.rename(inplace=True, columns=columns)
+
+df_prefs = df_prefs[ columns.values() ]   #TODO quick work around, might not be needed later
+
+df_prefs = df_prefs.apply(myfunc, axis=1)
+
+#Quick hack to remove columns which are no longer needed
+for i in range(1,6):
+    d = format_dict(dynamic_columns_template, i)
+    for jk, jv in d.items():
+        del df_prefs[jv ]
+
+dataframe_to_json_file(df_prefs.head() )
+
+# print ( dataframe_to_json(df_prefs.head()) )
 
 # df_merged = pd.merge(df_schools, df_prefs, left_on='name', right_on='preference_1_school_name')
 
